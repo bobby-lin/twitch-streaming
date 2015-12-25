03ea88d4308887921498ed6c9b8c250cae4c67ec
Bobby Lin <bobbylinjh@gmail.com>
2015-12-25 20:40:29 +0800

    Refactored $(document).ready in common.js

--- scripts/common.js
+++ scripts/common.js
@@ -4,23 +4,63 @@
  */
 
 $(document).ready(function() {
-    var users = ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb", 
-                 "thomasballinger","noobs2ninjas","MedryBW","beohoff","brunofin","comster404"];
-    for(var i = 0 ; i < users.length; i++) {
-        var url = "https://api.twitch.tv/kraken/channels/" + users[i];
-        var html = "";
-        $.getJSON(url, function(data) {
-            console.log(data);
-            if(data.logo !== null) {
-                html = "<li><img class='user-img' src='" + data.logo + "'>" + data.name + "</li>";
+    function addOnline(url_user,user) {
+        $.getJSON(url_user, function (data) {
+            description = data.status;
+            if (data.logo !== null) {
+                html = "<li><img class='user-img' src='" + data.logo + "'><span class='online-header'>"
+                    + data.name + "</span><br>" + description + "</br>" + "</li>";
             }
             else {
-                html = "<li><img class='user-img' src='http://placehold.it/50x50'>" + data.name + "</li>";
+                html = "<li><img class='user-img' src='http://placehold.it/50/95A5A6/white?text=No+logo'>"
+                    + "<span class='online-header'>" + data.name + "</span></li>";
             }
             $("#user-list").append(html);
-        }).fail(function(err) {
-            console.log(err);
+        }).fail(function (err) {
+            addClosedAccount(user)
         });
-        
     }
+    
+    function addOffline(url_user,user) {
+        $.getJSON(url_user, function (data) {
+            var description = data.status;
+            if (data.logo !== null) {
+                html = "<li><img class='user-img' src='" + data.logo + "'><span class='offline-header'>"
+                    + data.name + "</span>" + "</li>";
+            }
+            else {
+                html = "<li><img class='user-img' src='http://placehold.it/50/95A5A6/white?text=No+logo'>"
+                    + "<span class='offline-header'>" + data.name + "</span></li>";
+            }
+            $("#user-list").append(html);
+        }).fail(function (err) {
+            addClosedAccount(user)
+        });
+    }
+    
+    function addClosedAccount(user) {
+        var span_user = "<span class='closed-header'>" + user +" (Closed)</span>";
+        html = "<li><img class='user-img' src='http://placehold.it/50?text=No+logo'>"
+            + span_user + "</li>";
+        $("#user-list").append(html);
+    }
+    
+    var users = ["freecodecamp", "storbeck", "terakilobyte", "habathcx", "RobotCaleb",
+        "thomasballinger", "noobs2ninjas", "MedryBW", "beohoff", "brunofin", "comster404"];
+    users.forEach(function (user) {
+        var url = "https://api.twitch.tv/kraken/";
+        var html = "";
+        var url_stream = url + "streams/" + user + "?callback=?";
+        var url_user = url + "channels/" + user;
+        $.getJSON(url_stream, function (json) {
+            var description = "";
+            if (json.stream === null) {
+                console.log("Offline: " + user);
+                addOffline(url_user,user)
+            } else {
+                console.log("Now streaming: " + user);
+                addOnline(url_user,user)
+            }
+        });
+    });
 });
